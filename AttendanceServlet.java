7e78a1a88098bc62d0c56a595c7f1396072f962c
dnbanjali25-tech <dnbanjali25@gmail.com>
2026-05-04 14:29:40 +0530

    Update AttendanceServlet.java

--- AttendanceServlet.java
+++ AttendanceServlet.java
@@ -1,1 +1,31 @@
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+public class AttendanceServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String studentName = req.getParameter("studentName");
+        String date = req.getParameter("date");
+        String status = req.getParameter("status");
+
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO attendance(student_name,date,status) VALUES(?,?,?)"
+            );
+
+            ps.setString(1, studentName);
+            ps.setString(2, date);
+            ps.setString(3, status);
+
+            ps.executeUpdate();
+            res.sendRedirect("dashboard.jsp");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
